11dc84aef71e61e5f3414cce005e832760d744b9
Daid <daid303@gmail.com>
2026-05-28 12:20:14 +0200

    Add pin mapping

---
+++ sw/firmware/inc/pins.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#define PIN_GB_A0  0
+#define PIN_GB_A15 15
+
+#define PIN_GB_RD  16
+#define PIN_GB_WR  17
+#define PIN_GB_CS  18
+
+#define PIN_MBC_A0 19
+#define PIN_MBC_A1 20
+#define PIN_MBC_A2 21
+#define PIN_MBC_A3 22
+#define PIN_MBC_A4 23
+#define PIN_MBC_A5 24
+#define PIN_MBC_A6 25
+//MBC_A7 and MBC_A8 are not connected in V1
+#define PIN_MBC_A7 26
+#define PIN_MBC_A8 27
+
+#define PIN_MEM_CE 28
+#define PIN_MEM_OE 29
+#define PIN_MEM_WE 30
+
+#define PIN_GB_D0  31
+#define PIN_GB_D1  32
+#define PIN_GB_D2  33
+#define PIN_GB_D3  34
+#define PIN_GB_D4  35
+#define PIN_GB_D5  36
+#define PIN_GB_D6  37
+#define PIN_GB_D7  38
+
+#define PIN_BUS_EN 39
+
+#define PIN_GB_RST 40
+#define PIN_RUMBLE 41
+#define PIN_SDA    42
+#define PIN_SDL    43
+#define PIN_SD_MISO 44
+#define PIN_SD_CS   45
+#define PIN_SD_CLK  46
+#define PIN_SD_MOSI 47
